4ab4576370d01ec804465c8fa3d2009e5771c892
CarlosQA <carlostecnicowl@gmail.com>
2025-11-21 21:29:18 -0300

    vai

---
+++ sudoku/src/sudoku/Sudoku.java
@@ -0,0 +1,16 @@
+package sudoku;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Sudoku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
